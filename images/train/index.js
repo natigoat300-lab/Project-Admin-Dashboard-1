b013d6e2f529ff73f2db5b8c3caf7d345bc80cba
natigoat37 <natigoat300@gmail.com>
2026-05-25 14:29:21 +0300

    feat(ui): implement dashboard sidebar and header layout

---
+++ images/train/index.js
@@ -0,0 +1,4 @@
+const rock = document.querySelector(".rock");
+const paper = document.querySelector(".paper");
+const scissor = document.querySelector("scissor");
+
---
+++ images/train/practice/index.js
@@ -0,0 +1,17 @@
+function Person(name) {
+  this.name = name;
+}
+
+Person.prototype.sayName = function() {
+  console.log(`Hello, I'm ${this.name}!`);
+};
+
+function Player(name, marker) {
+  this.name = name;
+  this.marker = marker;
+}
+
+Player.prototype.getMarker = function() {
+  console.log(`My marker is '${this.marker}'`);
+};
+Object.getPrototypeOf(Player.prototype);
